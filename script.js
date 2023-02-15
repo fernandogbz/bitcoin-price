37107e71597acee4a41fd8e8e034de07ddee2046
Fernando Jimenez <fgjv06@gmail.com>
2023-02-14 23:37:28 -0300

    JS - bitcoin price in dollars added

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const usdAmount = document.getElementById('usd-amount');
 fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
   .then(response => response.json())
   .then(data => displayData(data));
+
+const displayData = data => {
+  const usd = data.bpi.USD.rate_float;
+  usdAmount.textContent = `$${usd} USD`;
+}
