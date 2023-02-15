e41ab706d67819a5f243af9d00873c7ff48aedc6
Fernando Jimenez <fgjv06@gmail.com>
2023-02-14 23:46:30 -0300

    JS - displayData() loop

--- script.js
+++ script.js
@@ -8,4 +8,12 @@ fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
 const displayData = data => {
   const usd = data.bpi.USD.rate_float;
   usdAmount.textContent = `$${usd} USD`;
+  const totalDollarItems = Math.trunc(usd/1000);
+  for(let i = 0; i < totalDollarItems; i++) {
+    const newDollar = document.createElement('div');
+    newDollar.setAttribute("style", `animation-delay:.${10 + i}s;`);
+    newDollar.textContent = '$';
+    newDollar.setAttribute('class', 'coin dollar-item');
+    dollarContainer.appendChild(newDollar);
+  }
 }
